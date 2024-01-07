b1a6f6b29ed135cdd910f68979637a770e39cc62
riddhi-webds <100923091+riddhi-webds@users.noreply.github.com>
2024-01-07 13:24:19 +0000

    variables nd datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 24673
+let accountEmail ="riddhi@gmail.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2 NOt allowed
+
+/* prefer not to use var
+bcz of issues in block scope nd functional scope
+*/
+
+
+accountEmail ="rj@d.com"
+accountPass = "21121"
+accountCity = "Bangaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
