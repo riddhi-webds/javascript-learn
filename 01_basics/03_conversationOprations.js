f47c0d93b9e8554b28d86e4af5d7e5e345f41edc
riddhi-webds <100923091+riddhi-webds@users.noreply.github.com>
2024-01-07 14:16:58 +0000

    conversation datatypes

---
+++ 01_basics/03_conversationOprations.js
@@ -0,0 +1,34 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+let score = "RIdhi"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan 
+// true => 1; false => 0
+
+// let isLoggdId = 1
+// let isLoggdId = ""
+let isLoggdId = "Riddhi"
+let booleanIsLoggedIn = Boolean(isLoggdId)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// " => false"
+// "Riddhi" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
