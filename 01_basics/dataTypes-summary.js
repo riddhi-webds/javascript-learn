1accec55e7f603dd9cec3073bf808bcfb49ea0d8
riddhi-webds <100923091+riddhi-webds@users.noreply.github.com>
2024-01-08 05:17:46 +0000

    assignment done

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -2,7 +2,7 @@
 
 // 7 types : string, Number, boolean, null, undefined, symbol,  BigInt
 
-const scroe = 100
+const score = 100
 const scoreValue = 100.2
 
 const isLoggedIn = false
@@ -11,9 +11,9 @@ let userEmail;
 
 const id = Symbol('123')
 const anoutherId = Symbol('123')
-console.log(id === anoutherId);
+// console.log(id === anoutherId);
 
-// const bigNumber = 2347647432n 
+const bigNumber = 2347647432n 
 
 // reference (non-primitive)
 
@@ -25,8 +25,28 @@ let myObj = {
     age: 24,
 }
 
-const myFunction = function(){
+const myFunction = function () {
     console.log("Hello world");
 }
 
-console.log(typeof myFunction);
+console.log(typeof bigNumber);
+
+// score => number
+// scoreValue => number
+// isLoggedIn => boolean
+// outSideTemp => object
+// userEmail => undefined
+// id => symbol
+// anoutherId => symbol
+// heros => object
+// myObj => object
+// myFunction =>function
+// bigNumber => bigint
+
+
+
+
+
+// +++++++++++++++++++
+
+// stack (primitive), heap (non-primitive) memory
