d0032957dbc9e6bf017be1b158bbf4bbeb2d6eb9
riddhi-webds <100923091+riddhi-webds@users.noreply.github.com>
2024-03-02 16:52:42 +0000

    guess number project

---
+++ 07_projects/04_guess_number/myjs.js
@@ -0,0 +1,44 @@
+const randomNuber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const resultParas = document.querySelector('.resultParas');
+
+const p = document.createElement('p')
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if (playGame) {
+    submit.addEventListener('click', function (e) {
+        e.preventDefault()
+    });
+
+    function validateGuess(guess) {
+
+    }
+
+    function checkGuess(guess) {
+
+    }
+
+    function displayGuess(guess) {
+
+    }
+
+    function displayMessage(message) {
+
+    }
+
+    function endGame() {
+
+    }
+
+    function newGame() {
+
+    }
